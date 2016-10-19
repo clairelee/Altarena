d134d3214b4db135a097f0391eee17772eae416d
Claire Lee <lee.claire@berkeley.edu>
2016-10-19 03:37:00 +0000

    fix facebook authentication

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['883535568446847'], ENV['b7b37b9531262103f078bfb60eb724a9']
+  provider :facebook, '883535568446847', 'b7b37b9531262103f078bfb60eb724a9'
 end
