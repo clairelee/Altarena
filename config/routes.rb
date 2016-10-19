a907cd779764ed7962f435bb8b1c48661bc82af9
John Ho <johnhohjy@gmail.com>
2016-10-19 06:16:37 +0000

    new note

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-    resources :notes
     
     get 'auth/:provider/callback', to: 'sessions#create'
     get 'auth/failure', to: redirect('/')
     get 'signout', to: 'sessions#destroy', as: 'signout'
 
     resources :sessions, only: [:create, :destroy]
-    resource :home, only: [:show]
-
+    resources :home, only: [:show]
+    resources :notes
+    
     root to: "home#show"
 end
 
