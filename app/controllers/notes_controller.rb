a4a39965c2ea480b74c125b9280f9e721656bce6
John Ho <johnhohjy@gmail.com>
2016-10-19 04:34:44 +0000

    notes

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,19 @@
+class NotesController < ApplicationController
+    def new
+    end
+    
+    def show
+        @note = Note.find(params[:id])
+    end
+    def create
+        @note = Note.new(note_params)
+ 
+        @note.save
+        redirect_to @note
+    end
+    
+    private
+        def note_params
+            params.require(:note).permit(:name, :description)
+        end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161018214716) do
+ActiveRecord::Schema.define(version: 20161019042310) do
+
+  create_table "notes", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "provider"
