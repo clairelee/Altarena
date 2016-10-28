0e233485b425d57f812278f5aa66f4d76c8ed5f3
Gun Soo Kim <gunsoo02@berkeley.edu>
2016-10-28 20:05:04 +0000

    Create notes, View notes, Edit notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,17 +2,71 @@ class NotesController < ApplicationController
     def new
     end
     
+    def new_musician
+        id = params[:id]
+        @note = Note.find(id)
+    end
+    
+    def new_actor
+        id = params[:id]
+        @note = Note.find(id)
+    end
+    
     def index
         @notes = Note.all
     end
     
     def show
         @note = Note.find(params[:id])
+        @note_fields = note_fields
+        
+    end
+    
+    def edit
+        @note = Note.find(params[:id])
+        puts "asfasdfa e"
+        puts @note.rating
+        puts @note.attitude
+        @note_fields = note_fields
+    end
+    
+    def update
+        @note = Note.find(params[:id])
+        if @note.update(note_params)
+            flash[:notice] = "#{@note.name} was successfully updated."
+            redirect_to note_path(@note)
+        else
+            render 'edit'
+        end
     end
     
     def create
         @note = Note.new(note_params)
         @note.save
+        if @note.role == "Musician"
+            redirect_to notes_new_musician_path(:id => @note.id)
+        elsif @note.role == "Actor" or @note.role == "Actress"
+            redirect_to notes_new_actor_path(:id => @note.id)
+        else
+            redirect_to note_path(@note)
+        end
+    end
+    
+    def create_musician
+        id = params[:id]
+        @note = Note.find(id)
+        @note.update(note_params)
+        @note.save
+        
+        redirect_to note_path(@note)
+    end
+    
+    def create_actor
+        id = params[:id]
+        @note = Note.find(id)
+        @note.update(note_params)
+        @note.save
+        
         redirect_to note_path(@note)
     end
     
@@ -25,6 +79,12 @@ class NotesController < ApplicationController
     
     private
         def note_params
-            params.require(:note).permit(:name, :role, :description)
+            params.require(:note).permit(:name, :production, :role, :description, :rating, :attitude, :star_sub, :musical_maturity, :reads_music, :harmony_singer)
         end
+        
+        def note_fields
+            {"attitude"=>{4=>"Brings joy to every rehearsal", 3=>"Professional and easy to work with", 2=>"Sometimes needs managing", 1=>"Difficult"}, "rating"=>{5=>"Yes!!", 4=>"Yes", 3=>"I won't cast them, but I won't object", 2=>"Rather not", 1=>"Definitely not"}, "star_sub"=>{true=>"Yes", false=>"No"},"reads_music"=>{3=>"Very well", 2=>"Can work on their own, but not sightread", 1=>"No"}, "harmony_singer"=>{4=>"Pro/Veteran", 3=>"Solidly teachable", 2=>"Teachable but weak", 1=>"No"}, "musical_maturity"=>{4=>"Pro/Veteran", 3=>"Solid", 2=>"A few weaknesses but fine", 1=>"Green"}}
+        end
+        
+        
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
     get 'auth/failure', to: redirect('/')
     get 'signout', to: 'sessions#destroy', as: 'signout'
     get 'note_delete', to: 'notes#destroy'
+    get 'notes_new_musician', to: 'notes#new_musician'
+    get 'notes_new_actor', to: 'notes#new_actor'
+    post 'notes_create_musician', to: 'notes#create_musician'
+    post 'notes_create_actor', to: 'notes#create_actor'
 
     resources :sessions, only: [:create, :destroy]
     resources :home, only: [:show]
