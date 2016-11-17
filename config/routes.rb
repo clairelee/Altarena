9eef1837b2e186d61886e193ef6150795bcf4ecc
Gun Soo Kim <gunsoo02@berkeley.edu>
2016-11-17 01:10:43 +0000

    Added Add Production Button, fixed routes for production

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
     
-  resources :productions
     get 'auth/:provider/callback', to: 'sessions#create'
     get 'auth/failure', to: redirect('/')
     get 'signout', to: 'sessions#destroy', as: 'signout'
