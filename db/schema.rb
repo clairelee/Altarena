1ae77c34d23458128dae4f5b90a443b1d39b0a26
Claire Lee <claire4ever@gmail.com>
2016-11-17 00:37:09 +0000

    fix readme

--- db/schema.rb
+++ db/schema.rb
@@ -13,16 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20161116051903) do
 
-  create_table "identities", force: :cascade do |t|
-    t.string   "uid"
-    t.string   "provider"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "identities", ["user_id"], name: "index_identities_on_user_id"
-
   create_table "notes", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
