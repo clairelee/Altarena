a9155d5abb0c20d0193ee37f4275f60374ccc78b
Karan Das <karan.das@berkeley.edu>
2016-11-22 14:10:20 +0000

    ui changes to all notes views

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20161116051903) do
+
   create_table "notes", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
