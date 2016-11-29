5ee312692b6ffc363c94a7a34992875c65f6ea1b
Gun Soo Kim <gunsoo02@berkeley.edu>
2016-11-29 06:59:07 +0000

    Connect instruments to notes

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,8 +23,7 @@ User.all.each do |user|
     defaultNotes.each do |note|
       note = user.new_note(note)
       note.save
-      ni = note.notes_instruments.new(:note => note, :instrument => instruments.sample)
-      ni.save
+      new_notes_instrument = NotesInstrument.create(:note_id => note.id, :instrument_id => 1)
     end
     user.save
 end    
