bab9fc955fa0d79a7e327bb2ca902e224bf8864d
Karan Das <karan.das@berkeley.edu>
2016-11-29 06:53:54 +0000

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,32 +6,29 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Instrument.create([{ name: 'Violin' }, { name: 'Percussion'}, { name: 'Guitar' }, {name: 'Piano/Keyboards'}, {name: 'Harp'}, {name: 'Viola'}, {name: 'Cello'}, {name: 'Acoustic Bass'}, {name: 'Fender Bass'}, {name: 'Electric Guitar'}, {name: 'Flamenco Guitar'}, {name: 'Banjo'}, {name: 'Soprano Sax'}, {name: 'Alto Sax'}, {name: 'Tenor Sax'}, {name: 'Baritone Sax'}, {name: 'Bass Sax'}, {name: 'Oboe'}, {name: 'English Horn'}, {name: 'Bassoon'}, {name: 'Clarinet'}, {name: 'Alto Clarinet'}, {name: 'Bass Clarinet'}, {name: 'Flute'}, {name: 'Piccolo'}, {name: 'Alto Flute'}, {name: 'Bass Flute'}, {name: 'Harmonica'}, {name: 'Accordion'}, {name: 'Trumpet/Flugelhorn'}, {name: 'Horn'}, {name: 'Trombone'}, {name: 'Bass Trombone'}, {name: 'Tuba'}])
+instruments = Instrument.create([{ name: 'Violin' }, { name: 'Percussion'}, { name: 'Guitar' }, {name: 'Piano/Keyboards'}, {name: 'Harp'}, {name: 'Viola'}, {name: 'Cello'}, {name: 'Acoustic Bass'}, {name: 'Fender Bass'}, {name: 'Electric Guitar'}, {name: 'Flamenco Guitar'}, {name: 'Banjo'}, {name: 'Soprano Sax'}, {name: 'Alto Sax'}, {name: 'Tenor Sax'}, {name: 'Baritone Sax'}, {name: 'Bass Sax'}, {name: 'Oboe'}, {name: 'English Horn'}, {name: 'Bassoon'}, {name: 'Clarinet'}, {name: 'Alto Clarinet'}, {name: 'Bass Clarinet'}, {name: 'Flute'}, {name: 'Piccolo'}, {name: 'Alto Flute'}, {name: 'Bass Flute'}, {name: 'Harmonica'}, {name: 'Accordion'}, {name: 'Trumpet/Flugelhorn'}, {name: 'Horn'}, {name: 'Trombone'}, {name: 'Bass Trombone'}, {name: 'Tuba'}])
+
 profile = Profile.create(:name => "Ronald")
     profile.save
 production = Production.create(:name => "Lion King", :company => "Disney", :opening_date => "June 1st, 1995")
     production.save
+
+User.all.each do |user|
     
-    defaultNotes = [{:name => 'Ronald', :production_id => 'Lion King', :role => 'Musician', :instruments => 'Bass Flute',
+    defaultNotes = [{:name => 'Ronald', :production => production, :role => 'Musician',
                     :description => 'sight reading', :rating => '9', :attitude => 'Brings joy to every rehearsal', 
                     :star_sub => "Yes", :musical_maturity => 'Solid', :reads_music => true, :harmony_singer => true,
-                    :profile_id => profile_id}]
+                    :profile => profile}]
     
     defaultNotes.each do |note|
-      Notes.create!(note)
-      user.new_note(note)
+      note = user.new_note(note)
+      note.save
+      ni = note.notes_instruments.new(:note => note, :instrument => instruments.sample)
+      ni.save
     end
     user.save
-    
-    # defaultNotes.each do |note|
-    #   puts note["name"]
-    #   user.new_note(note)
-    # end
-    # user.save
-    # user.all_notes.each do |note|
-    #   puts "Here is a note " + note.name
-    # end
-    
+end    
+
     # default_notes = [
     #     { 
     #       name: "Billy",
@@ -48,3 +45,12 @@ production = Production.create(:name => "Lion King", :company => "Disney", :open
     #       profile_id: profile.id
     #     }
     # ]
+
+    # defaultNotes.each do |note|
+    #   puts note["name"]
+    #   user.new_note(note)
+    # end
+    # user.save
+    # user.all_notes.each do |note|
+    #   puts "Here is a note " + note.name
+    # end
