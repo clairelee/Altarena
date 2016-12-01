0058bc80afeb998151eaadb9991de9af6946a0e8
Claire Lee <claire4ever@gmail.com>
2016-12-01 04:18:11 +0000

    add before filter to combat URL hacking

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
   autocomplete :profile, :name
+  before_filter :require_login
   
   def current_user
     @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
+  
+  private
+
+  def require_login
+    unless current_user
+      redirect_to('/')
+    end
+  end
+  
+  
 end
