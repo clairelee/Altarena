4e505dc21505be0be68a9e895d252c2f2e79968d
Karan Das <karan.das@berkeley.edu>
2016-12-01 05:33:46 +0000

    third seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,10 @@
 
 # profile = Profile.create(:name => "Ray Charles", :photo_url => "http://www.mtv.com/crop-images/2013/09/04/Ray%20Charles%20GI.jpg", :resume_url => "")
 #     profile.save
-profile = Profile.create(:name => "Eric Walbridge", :photo_url => "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAARRAAAAJDE5NzQyM2Q4LWQyNzUtNDU2OC1iMDJmLTg0M2U5ZjE1MjQxOQ.jpg", :resume_url => "")
-    profile.save
-# profile = Profile.create(:name => "Buddy Rich", :photo_url => "http://cdn-x.drummerworld.com/pics/drumpics34/buddyrich500999.jpg", :resume_url => "")
+# profile = Profile.create(:name => "Eric Walbridge", :photo_url => "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAARRAAAAJDE5NzQyM2Q4LWQyNzUtNDU2OC1iMDJmLTg0M2U5ZjE1MjQxOQ.jpg", :resume_url => "")
 #     profile.save
+profile = Profile.create(:name => "Buddy Rich", :photo_url => "http://cdn-x.drummerworld.com/pics/drumpics34/buddyrich500999.jpg", :resume_url => "")
+    profile.save
 
 production1 = Production.create(:name => "Lion King 1.5", :company => "Disney", :opening_date => "June 23rd, 2017")
     production1.save
@@ -32,31 +32,31 @@ User.all.each do |user|
     #                 :profile => profile}]
 
                     
-    defaultNotes = [{:name => 'Eric Walbridge', :production_id => 1, :role => 'Actor/Actress',
-                :description => 'Eric is extremeley responsive to constructive criticism', :rating => 4, :attitude => 3,
-                :star_sub => true, :musical_maturity => 3, :reads_music => 1, :harmony_singer => 2,
-                :profile => profile},
-                {:name => 'Eric Walbridge', :production_id => 2, :role => 'Musician',
-                :description => 'Eric often gets caught up with the small details, which hinders the group at times', :rating => 3, :attitude => 4,
-                :star_sub => false, :musical_maturity => 2, :reads_music => 3, :harmony_singer => 1,
-                :profile => profile}]
+    # defaultNotes = [{:name => 'Eric Walbridge', :production_id => 1, :role => 'Actor/Actress',
+    #             :description => 'Eric is extremeley responsive to constructive criticism', :rating => 4, :attitude => 3,
+    #             :star_sub => true, :musical_maturity => 3, :reads_music => 1, :harmony_singer => 2,
+    #             :profile => profile},
+    #             {:name => 'Eric Walbridge', :production_id => 2, :role => 'Musician',
+    #             :description => 'Eric often gets caught up with the small details, which hinders the group at times', :rating => 3, :attitude => 4,
+    #             :star_sub => false, :musical_maturity => 2, :reads_music => 3, :harmony_singer => 1,
+    #             :profile => profile}]
 
     
-    # defaultNotes = [{:name => 'Buddy Rich', :production_id => 1, :role => 'Musician',
-    #                 :description => 'Buddy is an amazing drummer, never have any problems with him', :rating => 5, :attitude => 4,
-    #                 :star_sub => true, :musical_maturity => 4, :reads_music => 3, :harmony_singer => 1,
-    #                 :profile => profile},
-    #                 {:name => 'Buddy Rich', :production_id => 2, :role => 'Musician',
-    #                 :description => 'Buddy is not only a great musician, but he is also a pleasure to work with', :rating => 5, :attitude => 4,
-    #                 :star_sub => true, :musical_maturity => 4, :reads_music => 3, :harmony_singer => 1,
-    #                 :profile => profile}]
+    defaultNotes = [{:name => 'Buddy Rich', :production_id => 1, :role => 'Musician',
+                    :description => 'Buddy is an amazing drummer, never have any problems with him', :rating => 5, :attitude => 4,
+                    :star_sub => true, :musical_maturity => 4, :reads_music => 3, :harmony_singer => 1,
+                    :profile => profile},
+                    {:name => 'Buddy Rich', :production_id => 2, :role => 'Musician',
+                    :description => 'Buddy is not only a great musician, but he is also a pleasure to work with', :rating => 5, :attitude => 4,
+                    :star_sub => true, :musical_maturity => 4, :reads_music => 3, :harmony_singer => 1,
+                    :profile => profile}]
                     
     defaultNotes.each do |note|
       note = user.new_note(note)
       note.save!
     #   new_notes_instrument = NotesInstrument.create(:note_id => "#{note.id}", :instrument_id => "4")
-      new_notes_instrument = NotesInstrument.create(:note_id => "#{note.id}", :instrument_id => "3")
-    #   new_notes_instrument = NotesInstrument.create(:note_id => "#{note.id}", :instrument_id => "2")
+    #   new_notes_instrument = NotesInstrument.create(:note_id => "#{note.id}", :instrument_id => "3")
+      new_notes_instrument = NotesInstrument.create(:note_id => "#{note.id}", :instrument_id => "2")
     end
     user.save
 end    
